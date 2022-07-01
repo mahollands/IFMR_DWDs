df74fba246241bdac654d23d7511ce888e10fd96
Mark Hollands <m.hollands@sheffield.ac.uk>
2022-07-01 14:15:11 +0100

    Changed to dont_use_DWD set containing a few bad systems

--- fit_ifmr_outliers_errors.py
+++ fit_ifmr_outliers_errors.py
@@ -6,53 +6,11 @@ import numpy as np
 import emcee
 from misc import Taylor_Expand_DWD, pairwise
 from mcmc_functions import logpost_DWDs
+from DWD_sets import bad_DWDs_220701 as dont_use_DWDs
 
 N_CPU = 10
-Nwalkers, Nstep = 100, 2000
+Nwalkers, Nstep = 100, 1000
 ifmr_x = np.array([0.5, 1, 1.5, 2, 2.5, 3, 4, 6, 8])
-
-use_DWDs = {
-    "chain_runA_WDJ0855-2637AB.npy",
-    "chain_runA_WDJ1015+0806AB.npy", #!
-    "chain_runA_WDJ1019+1217AB.npy",
-    "chain_runA_WDJ1124-1234AB.npy",
-    "chain_runA_WDJ1254-0218AB.npy",
-    "chain_runA_WDJ1336-1620AB.npy",
-    "chain_runA_WDJ1346-4630AB.npy", #?
-    "chain_runA_WDJ1636+0927AB.npy",
-    "chain_runA_WDJ1827+0403AB.npy",
-    "chain_runA_WDJ1856+2916AB.npy",
-    "chain_runA_WDJ1953-1019AB.npy", #?
-    "chain_runA_WDJ2131-3459AB.npy",
-    "chain_runA_WDJ2142+1329AB.npy", #!
-    "chain_runB_WDJ1215+0948AB.npy",
-    "chain_runB_WDJ1313+2030AB.npy",
-    "chain_runB_WDJ1338+0439AB.npy", #!
-    "chain_runB_WDJ1339-5449AB.npy",
-    "chain_runB_WDJ1535+2125AB.npy", #?
-    "chain_runB_WDJ1729+2916AB.npy", #!
-    "chain_runB_WDJ1831-6608AB.npy", #?
-    "chain_runB_WDJ1859-5529AB.npy", #?
-    "chain_runB_WDJ1904-1946AB.npy", #?
-    "chain_runC_WDJ0002+0733AB.npy", #?
-    "chain_runC_WDJ0052+1353AB.npy",
-    "chain_runC_WDJ0215+1821AB.npy",
-    "chain_runC_WDJ0410-1641AB.npy",
-    "chain_runC_WDJ0510+0438AB.npy",
-    "chain_runC_WDJ2058+1037AB.npy",
-    "chain_runC_WDJ2242+1250AB.npy",
-    "chain_runD_WDJ0920-4127AB.npy",
-    "chain_runD_WDJ1014+0305AB.npy",
-    "chain_runD_WDJ1100-1600AB.npy",
-    "chain_runD_WDJ1336-1620AB.npy",
-    "chain_runD_WDJ1445-1459AB.npy",
-    "chain_runD_WDJ1804-6617AB.npy",
-    "chain_runD_WDJ1929-3000AB.npy",
-    "chain_runD_WDJ2007-3701AB.npy",
-    "chain_runD_WDJ2026-5020AB.npy",
-    "chain_runD_WDJ2100-6011AB.npy",
-    "chain_runD_WDJ2150-6218AB.npy",
-}
 
 ###########################################################################
 # MCMC starts here
@@ -82,5 +40,6 @@ if __name__ == "__main__":
     with open("DWDs_Teffs_loggs.dat", 'rb') as F:
         DWDs = pickle.load(F)
     DWDs = {name : Taylor_Expand_DWD(DWD) for name, DWD in DWDs.items() \
-        if name in use_DWDs}
+        #if name not in use_DWDs}
+        if name not in dont_use_DWDs}
     run_MCMC(DWDs)
