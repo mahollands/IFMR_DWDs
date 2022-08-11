574b104adfb32541288fc3342daf6477f53a1e34
Mark Hollands <m.hollands@sheffield.ac.uk>
2022-08-11 11:48:40 +0100

    Priors available for monotonic mass loss

--- fit_ifmr_outliers_errors.py
+++ fit_ifmr_outliers_errors.py
@@ -5,14 +5,15 @@ import numpy as np
 import emcee
 from IFMR_tools import pairwise
 from mcmc_functions import logpost_DWDs
-from DWD_sets import bad_DWDs_220714 as dont_use_DWDs
+from DWD_sets import bad_DWDs_220809 as dont_use_DWDs
 from DWD_class import load_DWDs
 
 N_CPU = 10
-Nwalkers, Nstep = 1000, 1000
+Nwalkers, Nstep = 1000, 10000
 f_MCMC_out = "IFMR_MCMC_outliers"
-ifmr_x = np.array([0.5, 1, 1.5, 2, 2.5, 3, 4, 5, 6, 8])
+ifmr_x = np.array([0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 8])
 #ifmr_x = np.array([0.5, 2, 4, 8])
+ifmr_y_ = (1.4-0.4)/(8-0.5) * (ifmr_x-0.5) + 0.4
 
 ###########################################################################
 # MCMC starts here
@@ -22,26 +23,13 @@ def run_MCMC(DWDs):
 
     Mf_ranges = np.linspace(0, 1.4, len(ifmr_x)+1)
     pos0 = np.array([
-        #np.random.beta(3.0, 3.0, Nwalkers), #P_weird
-        np.random.beta(14.0, 6.0, Nwalkers), #P_weird
+        np.random.beta(3.0, 3.0, Nwalkers), #P_weird
         np.random.rayleigh(5.0, Nwalkers), #sig_weird
         np.random.rayleigh(0.02, Nwalkers), #Teff_err
         np.random.rayleigh(0.02, Nwalkers), #logg_err
-    ] + [np.random.uniform(*x01, Nwalkers) for x01 in pairwise(Mf_ranges)]).T
-    #] + [np.random.uniform(0, Mi, Nwalkers) for Mi in ifmr_x]).T
-    #] + [
-    #    np.random.normal(0.05, 0.01, Nwalkers), #IFMR
-    #    np.random.normal(0.55, 0.01, Nwalkers), #IFMR
-    #    np.random.normal(0.63, 0.01, Nwalkers), #IFMR
-    #    np.random.normal(0.65, 0.01, Nwalkers), #IFMR
-    #    np.random.normal(0.71, 0.01, Nwalkers), #IFMR
-    #    np.random.normal(0.74, 0.01, Nwalkers), #IFMR
-    #    np.random.normal(0.88, 0.01, Nwalkers), #IFMR
-    #    np.random.normal(0.90, 0.01, Nwalkers), #IFMR
-    #    np.random.normal(0.94, 0.01, Nwalkers), #IFMR
-    #    np.random.normal(1.31, 0.01, Nwalkers), #IFMR
-    #]).T
-    #    
+    ] + [np.random.normal(mf, 0.01, Nwalkers) for mf in ifmr_y_] #IFMR
+    ).T
+        
     Ndim = pos0.shape[1]
 
     ##Run MCMC
