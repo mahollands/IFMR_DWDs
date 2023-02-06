bc5b8992e6e61afc8157ba82f59365f4ef5a583c
Mark Hollands <m.hollands@sheffield.ac.uk>
2023-02-06 16:28:06 +0000

    Updated data to use runE

--- DWD_sets.py
+++ DWD_sets.py
@@ -125,3 +125,19 @@ bad_DWDs_220809 = {
     "runD_WDJ1804-6617AB", #B component v. low mass
     "runD_WDJ1929-3000AB", #B component has a He atm
 }
+
+bad_DWDs_230206 = {
+    "runA_WDJ1124-1234AB", #A component has a He atm
+    "runA_WDJ1636+0927AB", #poor fit to magnetic
+    "runA_WDJ1827+0403AB", #B component v. low mass
+    "runB_WDJ1729+2916AB", #B component has a He atm
+    "runC_WDJ0002+0733AB", #A component poor fit to phot due to magnetism
+    "runC_WDJ2122+3005AB", #B component v. low mass and A componet has He atm
+    "runD_WDJ1211-4551AB", #B component v. low mass
+    "runD_WDJ1350-5025AB", #A component has a He atm or strongly magnetic
+    "runD_WDJ1557-3832AB", #both components v. low mass
+    "runD_WDJ1804-6617AB", #B component v. low mass
+    "runD_WDJ1929-3000AB", #B component has a He atm
+    "runE_WDJ0240-3248AB", #A component magnetic with poor phot fit
+    "runE_WDJ1929-5313AB", #both components v. low mass
+}
--- fit_ifmr_outliers_errors.py
+++ fit_ifmr_outliers_errors.py
@@ -4,14 +4,13 @@ from multiprocessing import Pool
 import numpy as np
 import emcee
 from IFMR_stats import logpost_DWDs
-from DWD_sets import bad_DWDs_220809 as dont_use_DWDs
+from DWD_sets import bad_DWDs_230206 as dont_use_DWDs
 from DWD_class import load_DWDs
 
 N_CPU = 10
 Nwalkers, Nstep = 1000, 10000
 f_MCMC_out = "IFMR_MCMC_outliers_230120"
 ifmr_x = np.array([0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 8])
-#ifmr_x = np.array([0.5, 2, 4, 8])
 ifmr_y_ = (1.4-0.4)/(8-0.5) * (ifmr_x-0.5) + 0.4
 
 ###########################################################################
