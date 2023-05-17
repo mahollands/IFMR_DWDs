a9b7ba10ef672d0ea8a97a33bb3b86cea9fa6763
Mark Hollands <m.hollands@sheffield.ac.uk>
2023-05-17 12:09:00 +0100

    show option plots t_MS histogram

--- Mi_from_Mf.py
+++ Mi_from_Mf.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python
 """
 Tool for converting final masses to initial masses, including uncertainies
 in the final mass and the IFMR itself.
 """
 import argparse
+import ast
 import numpy as np
 import matplotlib.pyplot as plt
 from IFMR_tools import IFMR_cls, MSLT
 
-f_MCMC_out = "IFMR_MCMC_outliers"
+f_MCMC_out = "IFMR_MCMC_outliers_230511_extend01"
+BURN = -100
+THIN = 5
 
 parser = argparse.ArgumentParser()
 parser.add_argument("Mf_mean", type=float, \
@@ -35,7 +39,7 @@ with open("MCMC_meta.dat") as F:
         raise ValueError(f"Could not find meta data for {f_MCMC_out}")
 
 chain = np.load(f"MCMC_output/{f_MCMC_out}_chain.npy")
-final = chain[:,-1,:]
+final = chain[:,BURN::THIN,:].reshape((-1,chain.shape[-1]))
 
 Nskip = 4 if args.outliers else 2
 IFMRs = [IFMR_cls(ifmr_x, ifmr_y) for ifmr_y in final[:,Nskip:]]
@@ -56,6 +60,7 @@ Mi_str = "{:.2f}_-{:.2f}^+{:.2f}".format(Mi_pcs[1], *Mi_errs)
 t = MSLT(Mi)
 t_pcs = np.array([np.percentile(t, pc) for pc in (15.9, 50, 84.1)])
 if t_pcs[1] < 1:
+    t *= 1000
     t_pcs *= 1000
     t_unit = "Myr"
     t_fmt = "{:.0f}_-{:.0f}^+{:.0f}"
@@ -68,10 +73,17 @@ t_str = t_fmt.format(t_pcs[1], *t_errs)
 print(f"Mf : {Mf_str} Msun ==> Mi : {Mi_str} Msun, MS_t = {t_str} {t_unit}")
 
 if args.show:
-    plt.hist(Mi, bins=np.arange(0, 8, 0.1), density=True, histtype='step')
+    plt.hist(Mi, bins=np.arange(0, 8, 0.05), density=True, histtype='step')
     for Mi_pc in Mi_pcs:
         plt.axvline(Mi_pc, c='C1', ls=':')
-    plt.xlabel("$M_i [M_\odot]$")
-    plt.ylabel("$P(M_i)$")
+    plt.xlabel(r"$M_i [M_\odot]$")
+    plt.ylabel(r"$P(M_i)$")
     plt.xlim(0, 8)
+    plt.show()
+
+    plt.hist(t, bins=100, density=True, histtype='step')
+    for t_pc in t_pcs:
+        plt.axvline(t_pc, c='C1', ls=':')
+    plt.xlabel(rf"$\tau_\mathrm{{MS}}$ [{t_unit}]")
+    plt.ylabel(r"$P(\tau_\mathrm{MS})$")
     plt.show()
