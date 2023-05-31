941f38c77619565d9202bf01299a273e275d8f8f
Mark Hollands <m.hollands@sheffield.ac.uk>
2023-05-31 18:11:34 +0100

    Taylor expansions precomputed removing dependency on my MR_relation package

--- DWD_class.py
+++ DWD_class.py
@@ -3,7 +3,6 @@ Tools for Creating a DWD container object
 """
 import pickle
 import numpy as np
-from mh.MR_relation import M_from_Teff_logg, tau_from_Teff_logg
 
 class DWDcontainer:
     """
@@ -12,11 +11,11 @@ class DWDcontainer:
     measurement errors, the corresponding, masses/cooling ages, and their Jacobian
     matrix for adding systematic errors to the Teff/logg
     """
-    def __init__(self, name, Tg_vec, Tg_cov):
+    def __init__(self, name, Tg_vec, Tg_cov, TgMtau_expansion):
         self.name = name
         self.Tg_vec = Tg_vec
         self.Tg_cov = Tg_cov
-        vecMdtau, vecMtau, JacMdtau, JacMtau = Taylor_Expand_DWD(Tg_vec)
+        vecMdtau, vecMtau, JacMdtau, JacMtau = TgMtau_expansion
         self.vecMdtau = vecMdtau
         self.vecMtau = vecMtau
         self.JacMdtau = JacMdtau
@@ -76,52 +75,6 @@ class DWDcontainer:
     @property
     def M12(self):
         return self.vecMtau[:2]
-
-def Taylor_Expand_WD(T, g, dT=10, dg=0.001, H_thickness='thick'):
-    """
-    Pre-compute Taylor-Expansions for Masses and ages
-    """
-    T_vals = np.array([T, T+dT, T-dT, T,    T   ])
-    g_vals = np.array([g, g,    g,    g+dg, g-dg])
-
-    M_vals = M_from_Teff_logg(T_vals, g_vals, H_thickness)
-    M, M_p_dT , M_m_dT , M_p_dg , M_m_dg = M_vals
-    dM_dT = (M_p_dT - M_m_dT)/(2*dT)
-    dM_dg = (M_p_dg - M_m_dg)/(2*dg)
-    Taylor_M = M, dM_dT, dM_dg
-
-    tau_vals = tau_from_Teff_logg(T_vals, g_vals, H_thickness)
-    tau, tau_p_dT, tau_m_dT, tau_p_dg, tau_m_dg = tau_vals
-    dtau_dT = (tau_p_dT - tau_m_dT)/(2*dT)
-    dtau_dg = (tau_p_dg - tau_m_dg)/(2*dg)
-    Taylor_tau = tau, dtau_dT, dtau_dg
-
-    return Taylor_M, Taylor_tau
-
-def Taylor_Expand_DWD(Tg_vec):
-    """
-    Taylor expansion of M and tau for a double white dwarf system with measured
-    Teffs and loggs, returning the central M12/tau/dtau values and the
-    corresponding Jacobian matrices.
-    """
-    (T1, T2, g1, g2) = Tg_vec
-    (M1, dM1_dT1, dM1_dg1), (tau1, dtau1_dT1, dtau1_dg1) = Taylor_Expand_WD(T1, g1)
-    (M2, dM2_dT2, dM2_dg2), (tau2, dtau2_dT2, dtau2_dg2) = Taylor_Expand_WD(T2, g2)
-
-    vecMdtau = np.array([M1, M2, tau1-tau2])
-    JacMdtau = np.array([
-        [dM1_dT1, 0, dM1_dg1, 0],
-        [0, dM2_dT2, 0, dM2_dg2],
-        [dtau1_dT1, -dtau2_dT2, dtau1_dg1, -dtau2_dg2],
-    ])
-    vecMtau = np.array([M1, M2, tau1, tau2])
-    JacMtau = np.array([
-        [dM1_dT1, 0, dM1_dg1, 0],
-        [0, dM2_dT2, 0, dM2_dg2],
-        [dtau1_dT1, 0, dtau1_dg1, 0],
-        [0, dtau2_dT2, 0, dtau2_dg2],
-    ])
-    return vecMdtau, vecMtau, JacMdtau, JacMtau
 
 def load_DWDs(fname="DWDs_Teffs_loggs.pkl", use_set=None, exclude_set=None):
     """
