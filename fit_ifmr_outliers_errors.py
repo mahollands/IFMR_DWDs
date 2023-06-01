689c27f4e6f774dfa317b3d5e8eb1c8b036db52a
Mark Hollands <m.hollands@sheffield.ac.uk>
2023-06-01 14:28:28 +0100

    More informative prints at the start

--- fit_ifmr_outliers_errors.py
+++ fit_ifmr_outliers_errors.py
@@ -5,14 +5,14 @@ from multiprocessing import Pool
 import numpy as np
 import emcee
 from IFMR_stats import logpost_DWDs
-from DWD_sets import bad_DWDs_230511 as dont_use_DWDs
+from DWD_sets import bad_DWDs_230531 as dont_use_DWDs
 from DWD_class import load_DWDs
 from scipy.stats import invgamma
 from misc import load_fitted_IFMR, write_fitted_IFMR
 
 N_CPU = 10
 Nwalkers, Nstep = 1000, 10000
-f_MCMC_out = "IFMR_MCMC_outliers_Mch_230522"
+f_MCMC_out = "IFMR_MCMC_230601"
 ifmr_x = np.array([0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 8])
 ifmr_y_ = (1.4-0.4)/(8-0.5) * (ifmr_x-0.5) + 0.4
 
@@ -20,7 +20,6 @@ continue_run = False
 f_continue_from = ""
 
 ###########################################################################
-# MCMC starts here
 
 if continue_run:
     ifmr_x, chain, _ = load_fitted_IFMR(f_continue_from)
@@ -35,8 +34,9 @@ else:
     ).T
 Ndim = pos0.shape[1]
 
-def run_MCMC(DWDs):
-    print(len(DWDs))
+def run_MCMC(DWDs, pos0):
+    print(f_MCMC_out)
+    print(f"Fitting IFMR with {len(DWDs)} DWDs")
 
     ##Run MCMC
     with Pool(N_CPU) as pool:
@@ -48,4 +48,4 @@ def run_MCMC(DWDs):
 
 if __name__ == "__main__":
     DWDs = load_DWDs(exclude_set=dont_use_DWDs)
-    run_MCMC(DWDs)
+    run_MCMC(DWDs, pos0)
